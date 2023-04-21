65b95d26b19064a731a4890aa26267930ce09f3e
Rafael Giuriatto <rafagiuriatto@hotmail.com>
2023-04-21 18:52:51 -0300

    CRUD

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from . import views
+from .views import ListTask, DetailTask, CreateTask, UpdateTask, DeleteTask
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', ListTask.as_view(), name='index'),
+    path('task/<int:pk>', DetailTask.as_view(), name='task'),
+    path('create/', CreateTask.as_view(), name='create'),
+    path('edit/<int:pk>', UpdateTask.as_view(), name='edit'),
+    path('delete/<int:pk>', DeleteTask.as_view(), name='delete')
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,40 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from .models import Task
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 
-def index(request):
-    return render(request, 'main/index.html')
+class ListTask(ListView):
+    model = Task
+    context_object_name = 'tasks'
+    template_name = 'main/index.html'
+
+
+class DetailTask(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'main/task.html'
+
+
+class CreateTask(CreateView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'main/create.html'
+    fields = '__all__'
+    success_url = reverse_lazy('index')
+
+
+class UpdateTask(UpdateView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'main/create.html'
+    fields = '__all__'
+    success_url = reverse_lazy('index')
+
+
+class DeleteTask(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('index')
